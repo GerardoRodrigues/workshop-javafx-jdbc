a30b334eb1fd956469c5b00c35a35da5aa5eceff
Gerardo Rodrigues <gerardofilho56@gmail.com>
2021-10-13 18:03:33 -0300

    Acao como parametro (Consumer)

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,37 +36,22 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
 	@Override
 	public synchronized void initialize(URL url, ResourceBundle rb) {	
 	}
 	
-	public synchronized void loadView(String path) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
-			VBox newVBox = loader.load();
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			Node mainMenu = mainVBox.getChildren().get(0);
-			mainVBox.getChildren().clear();
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-		}
-		catch(IOException e) {
-			Alerts.showAlert("IOException", "Error loagind view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	public synchronized void loadView2(String path) {
+	public synchronized <T> void loadView(String path, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
 			VBox newVBox = loader.load();
@@ -78,9 +64,8 @@ public class MainViewController implements Initializable{
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
-			DepartmentListController controller = loader.getController();
-			controller.setService(new DepartmentService());
-			controller.updateTableView();
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 		}
 		catch(IOException e) {
 			Alerts.showAlert("IOException", "Error loagind view", e.getMessage(), AlertType.ERROR);
